464ff4f31a830d84a016db397d52161d5798c0a6
Manojna <apple@apples-MacBook-Pro-2.local>
2025-02-06 23:25:02 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/manojna-portfolio-",
+  base: "/manojna-portfolio",
   css: {
     modules: {
       localsConvention: "camelCase",
