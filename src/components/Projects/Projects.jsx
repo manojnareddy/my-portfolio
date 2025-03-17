ae0ea377ed8af454fbab4eb6dab949ead834d182
manojnareddy <manojnareddy1098@gmail.com>
2025-03-17 22:51:31 +0100

    modified7

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-
 import styles from "./Projects.module.css";
-
 import projects from "../../data/projects.json";
 import { ProjectCard } from "./ProjectCard";
 
@@ -10,9 +8,11 @@ export const Projects = () => {
     <section className={styles.container} id="projects">
       <h2 className={styles.title}>Projects</h2>
       <div className={styles.projects}>
-        {projects.map((project, id) => {
-          return <ProjectCard key={id} project={project} />;
-        })}
+        {projects.map((project, id) => (
+          <div key={id} className={styles.projectCard}>
+            <ProjectCard project={project} />
+          </div>
+        ))}
       </div>
     </section>
   );
